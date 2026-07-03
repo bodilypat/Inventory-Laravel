ca9e8e8492cbe955cbe151f97f3d102d48bd2dbd
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-03 07:45:36 -0700

    Add index.js for inventory component exports
    Created an index file to export various inventory components.

---
+++ frontend/src/components/inventory/index.js
@@ -0,0 +1,18 @@
+//File: src/components/inventory/index.js 
+export { default as ProductCard } from './ProductCard';
+export { default as ProductForm } from './ProductForm';
+export { default as ProductTable } from './ProductTable';
+export { default as CategoryForm } from './CategoryForm';
+export { default as SupplierForm } from './SupplierForm';
+export { default as CustomerForm } from './CustomerForm';
+export { default as PurchaseForm } from './PurchaseForm';
+export { default as PurchaseTable } from './PurchaseTable';
+export { default as SaleForm } from './SaleForm';
+export { default as SaleTable } from './SaleTable';
+export { default as InventoryTable } from './InventoryTable';
+export { default as StockBadge } from './StockBadge';
+export { default as StockStatus } from './StockStatus';
+export { default as StockMovementTable } from './StockMovementTable';
+export { default as Filters } from './Filters';
+export { default as SummaryCards } from './SummaryCards';
+export { default as DashboardCards } from './DashboardCards';
