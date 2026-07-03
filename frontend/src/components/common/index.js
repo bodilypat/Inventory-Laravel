e8c7be1d6363dbb84a0e97421b34d76a0f607c8e
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-03 07:42:11 -0700

    Create index.js for common component exports

---
+++ frontend/src/components/common/index.js
@@ -0,0 +1,23 @@
+//File : src/components/common/index.js
+
+export { default } from './Button';
+export { default } from './Input';
+export { default } from './Select';
+export { default } from './Textarea';
+export { default } from './Checkbox';
+export { default } from './Radio';
+export { default } from './ButtonGroup';
+export { default } from './Card';
+export { default } from './Modal';
+export { default } from './Table';
+export { default } from './Badge';
+export { default } from './Alert';
+export { default } from './Toast';
+export { default } from './Loader';
+export { default } from './Spinner';
+export { default } from './SearchBar';
+export { default } from './Pagination';
+export { default } from './Breadcrumb'; 
+export { default } from './Avatar';
+export { default } from './EmptyState';
+export { default } from './ConfirmDialog'
