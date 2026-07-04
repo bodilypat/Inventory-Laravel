5b1f944eb7dc267fca7cd1e1d58284b70c789535
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-03 17:44:33 -0700

    Create index.js

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,7 @@
+//File: src/route/index.js 
+export { default as AppRoutes } from "./AppRoute";
+export { default as PrivateRoute } from "./PrivateRoute";
+export { default as PublicRoute } from "./PublicRoute";
+export { default as RoleRoute } from "./RoleRoute";
+export * from "./routePaths";
+
