4765ecd5b1c19fbdcbde72214fa419961f60ae63
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-03 17:42:05 -0700

    Create index.js

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,11 @@
+//File: src/constants/index.js 
+export * from "./api";
+export * from "./app";
+export * from "./auth";
+export * from "./routes";
+export * from "./roles";
+export * from "./inventory";
+export * from "./message";
+export * from "./status";
+export * from "./validation";
+
